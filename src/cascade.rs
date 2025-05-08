50d681bdb2169b1db2b9e1572c0da4bf2a257bd6
iancleary <github@iancleary.me>
2025-05-08 05:30:23 -0700

    Move db <-> linear to rfconversions, use super:: in tests

--- src/cascade.rs
+++ src/cascade.rs
@@ -14,14 +14,6 @@ pub struct SignalNode {
     cumulative_gain: f64, // cumulative, dB (set to 0 at start)
 }
 
-pub fn db_to_linear(value: f64) -> f64 {
-    10.0_f64.powf(value / 10.0)
-}
-
-pub fn linear_to_db(value: f64) -> f64 {
-    10.0 * f64::log10(value)
-}
-
 pub fn cascade(input_power: f64, block1: GainBlock) -> f64 {
     input_power + block1.gain
 }
@@ -29,7 +21,7 @@ pub fn cascade(input_power: f64, block1: GainBlock) -> f64 {
 pub fn cascade_node(signal: SignalNode, block1: GainBlock) -> SignalNode {
     let output_node_name = block1.name + " Output";
     let block_noise_temperature = rfconversions::noise::noise_temperature_from_noise_figure(block1.noise_figure);
-    let cumulative_gain_linear = db_to_linear(signal.cumulative_gain) + db_to_linear(block1.gain);
+    let cumulative_gain_linear = rfconversions::power::db_to_linear(signal.cumulative_gain) + rfconversions::power::db_to_linear(block1.gain);
     SignalNode {
         name: output_node_name,
         power: signal.power + block1.gain,
@@ -40,20 +32,15 @@ pub fn cascade_node(signal: SignalNode, block1: GainBlock) -> SignalNode {
 
 #[cfg(test)]
 mod tests {
-    use crate::cascade::cascade;
-    use crate::cascade::cascade_node;
-    use crate::cascade::GainBlock;
-    use crate::cascade::SignalNode;
-
     #[test]
     fn one_part() {
         let input_power: f64 = -30.0;
-        let amplifier = GainBlock {
+        let amplifier = super::GainBlock {
             name: "Simple Amplifier".to_string(),
             gain: 10.0,
             noise_figure: 3.0,
         };
-        let output_power = cascade(input_power, amplifier);
+        let output_power = super::cascade(input_power, amplifier);
 
         assert_eq!(output_power, -20.0);
     }
@@ -61,18 +48,18 @@ mod tests {
     #[test]
     fn one_part_node() {
         let input_power: f64 = -30.0;
-        let input_node = SignalNode {
+        let input_node = super::SignalNode {
             name: "Input".to_string(),
             power: input_power,
             noise_temperature: 290.0,
             cumulative_gain: 0.0, // starting/initial/input node of cascade
         };
-        let amplifier = GainBlock {
+        let amplifier = super::GainBlock {
             name: "Simple Amplifier".to_string(),
             gain: 10.0,
             noise_figure: 3.0,
         };
-        let output_node = cascade_node(input_node, amplifier);
+        let output_node = super::cascade_node(input_node, amplifier);
 
         assert_eq!(output_node.power, -20.0);
         assert_eq!(output_node.name, "Simple Amplifier Output");
@@ -84,19 +71,19 @@ mod tests {
     #[test]
     fn one_part_lna_node() {
         let input_power: f64 = -30.0;
-        let input_node = SignalNode {
+        let input_node = super::SignalNode {
             name: "Input".to_string(),
             power: input_power,
             noise_temperature: 290.0,
             cumulative_gain: 0.0, // starting/initial/input node of cascade
         };
-        let amplifier = GainBlock {
+        let amplifier = super::GainBlock {
             name: "Low Noise Amplifier".to_string(),
             gain: 30.0,
             noise_figure: 3.0,
         };
        
-        let output_node = cascade_node(input_node, amplifier);
+        let output_node = super::cascade_node(input_node, amplifier);
 
         assert_eq!(output_node.power, 0.0);
         assert_eq!(output_node.name, "Low Noise Amplifier Output");
@@ -108,27 +95,27 @@ mod tests {
     #[test]
     fn two_part_node() {
         let input_power: f64 = -30.0;
-        let input_node = SignalNode {
+        let input_node = super::SignalNode {
             name: "Input".to_string(),
             power: input_power,
             noise_temperature: 290.0,
             cumulative_gain: 0.0, // starting/initial/input node of cascade
         };
-        let amplifier = GainBlock {
+        let amplifier = super::GainBlock {
             name: "Low Noise Amplifier".to_string(),
             gain: 30.0,
             noise_figure: 3.0,
         };
-        let attenuator = GainBlock {
+        let attenuator = super::GainBlock {
             name: "Attenuator".to_string(),
             gain: -6.0,
             noise_figure: 6.0,
         };
-        let intermediate_node = cascade_node(input_node, amplifier);
+        let intermediate_node = super::cascade_node(input_node, amplifier);
 
         assert_eq!(intermediate_node.cumulative_gain, 30.0);
 
-        let output_node = cascade_node(intermediate_node, attenuator);
+        let output_node = super::cascade_node(intermediate_node, attenuator);
 
         assert_eq!(output_node.power, -6.0);
         assert_eq!(output_node.cumulative_gain, 24.0);
